927e74306417d68356cf90e54c568b045dde5d6c
Trong Nghia Tran <ovenlyvietnam@gmail.com>
2026-04-19 11:56:17 -0700

    Revert "add 4 corner food cutouts to hero, correct sizing and positioning"
    This reverts commit 9422a1f3e2115366cb5539658e1fddcaf4ae1ab4.

--- components/marketplace/MarketplaceClient.tsx
+++ components/marketplace/MarketplaceClient.tsx
@@ -531,16 +531,8 @@ export default function MarketplaceClient() {
       </header>
 
       {/* HERO */}
-      <div className="relative text-white py-14 px-4 overflow-hidden" style={{background: '#A32020'}}>
-        {/* Top-left: dumplings */}
-        <img src="https://res.cloudinary.com/dvnssii5h/image/upload/w_220,h_220,c_fit/dumplings_uftyxf.png" alt="" className="hidden md:block absolute top-0 left-0 select-none pointer-events-none" style={{width:'220px', transform:'translate(-48px, -24px)'}} />
-        {/* Top-right: pizza */}
-        <img src="https://res.cloudinary.com/dvnssii5h/image/upload/w_220,h_220,c_fit/pizza_rk3cau.png" alt="" className="hidden md:block absolute top-0 right-0 select-none pointer-events-none" style={{width:'220px', transform:'translate(48px, -24px)'}} />
-        {/* Bottom-left: banh mi */}
-        <img src="https://res.cloudinary.com/dvnssii5h/image/upload/w_280,h_180,c_fit/banhmi_yvelxm.png" alt="" className="hidden md:block absolute bottom-0 left-0 select-none pointer-events-none" style={{width:'280px', transform:'translate(-64px, 16px)'}} />
-        {/* Bottom-right: poke bowl */}
-        <img src="https://res.cloudinary.com/dvnssii5h/image/upload/w_220,h_220,c_fit/poke_s5txe1.png" alt="" className="hidden md:block absolute bottom-0 right-0 select-none pointer-events-none" style={{width:'220px', transform:'translate(48px, 24px)'}} />
-        <div className="relative z-10 max-w-3xl mx-auto text-center">
+      <div className="text-white py-14 px-4" style={{background: '#A32020'}}>
+        <div className="max-w-3xl mx-auto text-center">
           <h1 className="font-heading text-2xl md:text-3xl font-black mb-2 leading-tight">
             {lang === 'vi' ? 'Khám phá địa điểm ăn uống gần bạn' : 'Order food online near you'}
           </h1>
