1c09c8f8af59d3b47111a22360b6c96fc8fabca4
Trong Nghia Tran <ovenlyvietnam@gmail.com>
2026-04-20 16:15:41 -0700

    add Cloudinary to next/image remotePatterns

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'i.postimg.cc' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
+  },
 };
-
 module.exports = nextConfig;
