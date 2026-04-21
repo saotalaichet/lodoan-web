7100e6b7706255e934453eaf7757d7efff3ac2b8
Trong Nghia Tran <ovenlyvietnam@gmail.com>
2026-04-21 10:24:40 -0700

    fix robots.txt sitemap URL to use www

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/owner/', '/profile/', '/orders/', '/api/'],
       },
     ],
-    sitemap: 'https://lodoan.vn/sitemap.xml',
+    sitemap: 'https://www.lodoan.vn/sitemap.xml',
   };
 }
